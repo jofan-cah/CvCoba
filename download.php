4b5e375cb56d2c39be3049a6c6ce5d138a10f7fd
jofan-cah <202030247@mhs.udb.ac.id>
2021-08-10 11:28:13 +0700

    file download 2

--- download.php
+++ download.php
@@ -1,8 +1,7 @@
 <?php
 
 if (isset($_GET)) {
-    header('Content-Disposition: attachment; filename="coba.pdf"');
-    header('Content-Type: .pdf');
+    header('Content-Disposition: attachment;');
     readfile(__DIR__, '/pdf/coba.pdf');
     exit();
 }
