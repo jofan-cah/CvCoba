6b1a9c90a465800b15af0db847ee6a2f80e6139c
jofan-cah <202030247@mhs.udb.ac.id>
2021-08-10 12:02:51 +0700

    file download 4

--- coba.php
+++ coba.php
@@ -3,7 +3,7 @@
 header("Content-type:application/pdf");
 
 // It will be called downloaded.pdf
-header("Content-Disposition:attachment;filename='downloaded.pdf'");
+header("Content-Disposition:attachment;filename='coba.pdf'");
 
 // The PDF source is in original.pdf
 readfile("coba.pdf");
