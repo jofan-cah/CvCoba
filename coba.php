6a39610965e763e16dcc8a5f05034e56b2fd967c
jofan-cah <202030247@mhs.udb.ac.id>
2021-08-18 14:11:42 +0700

    menambhkan email

--- coba.php
+++ coba.php
@@ -1,3 +1,13 @@
-<iframe src="/coba.pdf" frameborder="0">
+<?php
+$nama = $_POST['nama'];
+$to = $_POST['email'];
+$subject = $_POST['subject'];
+$messages = $_POST['messages'];
 
-</iframe>
+$headers .= 'From: <jofan.fathurahman@gmail.com>' . "rn"; //bagian ini diganti sesuai dengan email dari pengirim
+$maill = mail($to, $subject, $messages, $headers . phpversion());
+if ($maill) {
+    echo "pengiriman berhasil";
+} else {
+    echo "pengiriman gagal";
+}
